3e34c14d301664b6cee04ee003f9f8d3ba3dde2b
kyleplo <31634240+kyleplo@users.noreply.github.com>
2021-12-30 19:18:47 -0500

    Update widget.js

--- apps/sleepwid/widget.js
+++ apps/sleepwid/widget.js
@@ -1,7 +1,7 @@
 (function(){
   Bangle.on("lock", function(on) {
     if(Bangle.isLocked()){
-      load("sleep.app.js");
+      load("sleepwid.app.js");
     }
   });
   WIDGETS.sleep={area:"tl",width:0,draw:function(w){}};
